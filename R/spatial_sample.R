7f993f691453208783a904a5c358c1e22142fac1
Jeffery Leirness <jeffery.leirness@noaa.gov>
2024-05-20 14:24:06 -0500

    Add feature that allows for input of SpatRaster object

--- R/spatial_sample.R
+++ R/spatial_sample.R
@@ -1,11 +1,11 @@
 #' Spatial sample
 #'
-#' Take a sample of the data according to the specified sampling design
+#' Take a sample of the data according to the specified sampling method
 #'
 #' @param data A `data.frame` object, ideally the output from
 #' spatialsample_prep().
 #' @param n The number of `data.frame` rows to select (i.e., the sample size).
-#' @param design Which sampling design should be used to select the rows?
+#' @param method Which sampling method should be used to select the rows?
 #'   * `"random"` (the default): random sampling via `dplyr::slice_sample()`.
 #'   * `"biased"`: random sampling of rows for which `bias_var` value is greater than
 #'   `bias_thresh` via `dplyr::slice_sample()`.
@@ -13,7 +13,7 @@
 #'   value of `strata_var` via `dplyr::slice_sample()`.
 #'   * `"clh"`: conditioned latin hypercube sampling via `clhs::clhs()`.
 #'   * `"balanced"`: spatially balanced sampling via `MBHdesign::quasiSamp()`.
-#'   * `"balanced_stratified"`: spatially balanced stratified sampling via
+#'   * `"balanced-stratified"`: spatially balanced stratified sampling via
 #'   `MBHdesign::quasiSamp()`.
 #' @param r A `SpatRaster` object specifying the
 #' @param bias_var description
@@ -21,25 +21,47 @@
 #' @param strata_var description
 #' @param clh_var description
 #' @param clh_iter A positive number, giving the number of iterations for the
-#' Metropolis-Hastings annealing process. If `design` is `"clh"`, this value is
+#' Metropolis-Hastings annealing process. If `method` is `"clh"`, this value is
 #' passed to the `iter` argument of `clhs::clhs()`.
 #'
 #' @return description
 #' @export
-spatial_sample <- function(data, n, design, r, bias_var, bias_thresh, strata_var, clh_var, clh_iter) {
-  if (design == "random") {
-    spatialsample_random(data, n = n)
-  } else if (design == "biased") {
-    spatialsample_biased(data, n = n, var = bias_var, thresh = bias_thresh)
-  } else if (design == "stratified") {
-    spatialsample_stratified(data, n = n, var = strata_var)
-  } else if (design == "clh") {
-    spatialsample_clh(data, n = n, var = clh_var, iter = clh_iter)
-  } else if (design == "balanced") {
-    spatialsample_balanced(data, n = n, r = r)
-  } else if (design == "balanced_stratified") {
-    spatialsample_balanced_stratified(data, n = n, r = r, var = strata_var)
+spatial_sample <- function(x, n, method, bias_var, bias_thresh, strata_var, clh_var, clh_iter, drop_na = TRUE, as_raster = FALSE) {
+
+  if (method %in% c("balanced", "balanced-stratified")) {
+    stopifnot("if `method` is 'balanced' or 'balanced-stratified'', then x must be a SpatRaster" = inherits(x, "SpatRaster"))
   }
+
+  if (inherits(x, "SpatRaster") & !(method %in% c("balanced", "balanced-stratified"))) {
+    df <- x |>
+      terra::as.data.frame(cells = TRUE, na.rm = drop_na) |>
+      tibble::as_tibble()
+  } else {
+    df <- x
+  }
+
+  if (method == "random") {
+    samp <- spatialsample_random(df, n = n)
+  } else if (method == "biased") {
+    samp <- spatialsample_biased(df, n = n, var = bias_var, thresh = bias_thresh)
+  } else if (method == "stratified") {
+    samp <- spatialsample_stratified(df, n = n, var = strata_var)
+  } else if (method == "clh") {
+    samp <- spatialsample_clh(df, n = n, var = clh_var, iter = clh_iter)
+  } else if (method == "balanced") {
+    samp <- spatialsample_balanced(x, n = n, drop_na = drop_na, as_raster = as_raster)
+  } else if (method == "balanced-stratified") {
+    samp <- spatialsample_balanced(x, n = n, strata_var = strata_var, drop_na = drop_na, as_raster = as_raster)
+  }
+
+  if (inherits(x, "SpatRaster") & as_raster) {
+    x_samp <- x
+    x_samp[-samp$cell] <- NA
+    x_samp
+  } else {
+    samp
+  }
+
 }
 #' Spatial sample of data
 #'
@@ -76,28 +98,38 @@ spatialsample_clh <- function(data, n, var, iter) {
     tibble::as_tibble() |>
     dplyr::slice(clh_samp$index_samples)
 }
-spatialsample_balanced <- function(data, n, r) {
-  r_ip <- r |>
-    terra::subset(subset = 1) |>
-    terra::not.na() * 1
+spatialsample_balanced <- function(r, n, strata_var = NULL, drop_na = TRUE, as_raster = FALSE) {
+  if (!is.null(strata_var)) {
+    df <- r |>
+      terra::as.data.frame(cells = TRUE, na.rm = drop_na) |>
+      tibble::as_tibble() |>
+      dplyr::group_by(.data[[strata_var]]) |>
+      dplyr::mutate(.w = 1 / dplyr::n()) |>
+      dplyr::ungroup()
+    r_ip <- r |>
+      terra::subset(subset = strata_var)  # converting NA's to 0's (as in `balanced` sample above) causes an issue with the raster resulting from terra::set.values() below
+    terra::set.values(r_ip, cells = df$cell, values = df$.w * 1e+07)
+    if (drop_na) r_ip[!terra::noNA(r)] <- NA
+  } else {
+    if (drop_na) {
+      r_ip <- terra::noNA(r) * 1
+    } else {
+      r_ip <- terra::subset(r, subset = 1)
+      terra::values(r_ip) <- 1
+    }
+  }
   ba_samp <- MBHdesign::quasiSamp(n = n, dimension = 2, inclusion.probs = r_ip) |>
     tibble::as_tibble()
-  data |>
-    tibble::as_tibble() |>
-    dplyr::filter(.cell %in% ba_samp$ID)
-}
-spatialsample_balanced_stratified <- function(data, n, r, var) {
-  df <- data |>
-    dplyr::group_by(.data[[var]]) |>
-    dplyr::mutate(.w = 1 / dplyr::n()) |>
-    dplyr::ungroup()
-  r_ip <- r |>
-    terra::subset(subset = 1)  # converting NA's to 0's (as in `balanced` sample above) causes an issue with the raster resulting from terra::set.values() below
-  terra::set.values(r_ip, cells = df$.cell, values = df$.w * 1e+07)
-  ba_samp <- MBHdesign::quasiSamp(n = n, dimension = 2, inclusion.probs = r_ip)
-  data |>
-    tibble::as_tibble() |>
-    dplyr::filter(.cell %in% ba_samp$ID)
+  if (as_raster) {
+    x_samp <- r
+    x_samp[-ba_samp$ID] <- NA
+    x_samp
+  } else {
+    r |>
+      terra::as.data.frame(cells = TRUE, na.rm = drop_na) |>
+      tibble::as_tibble() |>
+      dplyr::filter(cell %in% ba_samp$ID)
+  }
 }
 
 #' Spatial sample prep
